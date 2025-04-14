5019a18a7c58dfedf221374441dcfc2219589bae
Ayushi Kamthan <kamthanayushi102@gmail.com>
2025-04-14 15:49:37 +0530

    Made 'Publication' page responsive

--- src/pages/Publication.jsx
+++ src/pages/Publication.jsx
@@ -2,13 +2,13 @@
 export const Publication=()=>{
 
     return(
-        <div className=' flex flex-col justify-center items-center xl:py-32 lg:py-16'>
-            <h1 className="w-[80rem]  font-bold xl:text-5xl">Publication</h1>
-            <p className="w-[75rem] mt-10 text-xl"><span className="font-bold">The selected papers, after rigorous peer review, will have the opportunity to be published in Applications and Applied Mathematics (AAM, ISSN: 1932-9466), an ESCI-indexed journal, at no cost.</span> Additionally, selected research papers, after rigorous peer review, will be published in SCI/SCIE/ESCI/Scopus-indexed journals, subject to their Article Processing Charges (APC). The list of these journals will be provided soon, as communication with the journals is in progress.. 
+        <div className='flex flex-col container mx-auto xl:py-32 lg:py-16 py-8'>
+            <h1 className="font-bold xl:text-5xl lg:text-2xl text-lg">Publication</h1>
+            <p className="mt-10 text-sm lg:text-xl"><span className="font-bold">The selected papers, after rigorous peer review, will have the opportunity to be published in Applications and Applied Mathematics (AAM, ISSN: 1932-9466), an ESCI-indexed journal, at no cost.</span> Additionally, selected research papers, after rigorous peer review, will be published in SCI/SCIE/ESCI/Scopus-indexed journals, subject to their Article Processing Charges (APC). The list of these journals will be provided soon, as communication with the journals is in progress.. 
             </p>
             <div className='mt-10'>
-                <h1 className='w-[75rem] text-xl font-semibold'>Registration Link: <a className='text-blue-500 underline font-bold' href="https://forms.gle/c4JDVkMCTwBRFaZx5">Click here to Register</a></h1>
-                <h1 className='w-[75rem] text-xl font-semibold mt-3 '>Paper/Abstract submission Link: <a className='text-blue-500 underline font-bold' href="https://cmt3.research.microsoft.com/ICCASA2025/Submission/Manage">Click here to submit</a></h1>
+                <h1 className='text-sm lg:text-xl font-semibold'>Registration Link: <a className='text-blue-500 underline font-bold' href="https://forms.gle/c4JDVkMCTwBRFaZx5">Click here to Register</a></h1>
+                <h1 className='text-sm lg:text-xl font-semibold mt-3 '>Paper/Abstract submission Link: <a className='text-blue-500 underline font-bold' href="https://cmt3.research.microsoft.com/ICCASA2025/Submission/Manage">Click here to submit</a></h1>
             </div>
             
         </div>
